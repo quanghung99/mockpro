958414191f7b641058226f52581b418b33675d69
quanghung99 <hung99ktb@gmail.com>
2022-03-03 22:54:26 +0700

    profile_page ui completed

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,19 +1,35 @@
-import { Grid3x3, Textsms } from '@mui/icons-material';
+import {
+	AddCommentOutlined,
+	DeleteOutlined,
+	FavoriteBorderOutlined,
+	Grid3x3,
+	Textsms,
+	TextsmsOutlined,
+} from '@mui/icons-material';
 import CakeIcon from '@mui/icons-material/Cake';
 import SettingsIcon from '@mui/icons-material/Settings';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
-import { Avatar, Box, Button, Grid, Paper, Typography } from '@mui/material';
+import {
+	Avatar,
+	Box,
+	Button,
+	Container,
+	Grid,
+	Paper,
+	Typography,
+} from '@mui/material';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import {
 	articleAction,
 	selectArticleList,
 } from 'features/articles/articleSlice';
+import { commentActions } from 'features/comment/commentSlice';
 import {
 	profileActions,
 	selectProfileCurrent,
 } from 'features/profile/profileSlice';
 import React, { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation, useRouteMatch } from 'react-router-dom';
 import style from './style.module.scss';
 
 export interface ProfileProps {}
@@ -35,12 +51,19 @@ export default function Profile(props: ProfileProps) {
 		);
 	}, [dispatch]);
 
+	const handleAddComment = () => {
+		dispatch(commentActions.addComment);
+	};
+
+	const handleRemoveComment = () => {
+		dispatch(commentActions.removeComment);
+	};
+
 	return (
 		<Box className={style.rootProfile}>
-			<Box className={style.blackpath}></Box>
-
-			<Grid container className={style.grid} spacing={2}>
-				<Grid item lg={12}>
+			<Box className={style.header}>
+				<Box className={style.blackpath}></Box>
+				<Container>
 					<Paper elevation={3} className={style.headerProfile}>
 						<Box>
 							<Avatar
@@ -62,50 +85,150 @@ export default function Profile(props: ProfileProps) {
 									&nbsp; 3 march 2022
 								</Typography>
 							</Typography>
-							<Button
-								className={style.settingButton}
-								variant="contained"
-								color="primary"
-							>
-								<SettingsIcon /> &nbsp; Edit Profile
-							</Button>
+							<Link to="/editSetting">
+								<Button
+									className={style.settingButton}
+									variant="contained"
+									color="primary"
+								>
+									<SettingsIcon /> &nbsp; Edit Profile
+								</Button>
+							</Link>
 						</Box>
 					</Paper>
-				</Grid>
-				<Grid item lg={4}>
-					<Paper className={style.boardInfo}>
-						<Typography>
-							<TextSnippetIcon />
-							&nbsp; {articleByUser.length} &nbsp; post publish
-						</Typography>
-						<Typography>
-							<Textsms />
-							&nbsp; comments written
-						</Typography>
-						<Typography>
-							<Grid3x3 />
-							&nbsp; tags followed
-						</Typography>
-					</Paper>
-				</Grid>
+				</Container>
+			</Box>
+			<Container>
+				<Grid container className={style.grid} spacing={2}>
+					<Grid item lg={4}>
+						<Paper className={style.boardInfo}>
+							<Typography>
+								<TextSnippetIcon />
+								&nbsp; {articleByUser.length} &nbsp; post publish
+							</Typography>
+							<Typography>
+								<Textsms />
+								&nbsp; comments written
+							</Typography>
+							<Typography>
+								<Grid3x3 />
+								&nbsp; tags followed
+							</Typography>
+						</Paper>
+					</Grid>
 
-				<Grid item lg={8}>
-					<Box>
-						<Paper>
+					<Grid item lg={8}>
+						<Paper className={style.ContainArticle}>
 							<Box className={style.userInfo}>
 								<Avatar
-									// className={}
 									sx={{ width: '32px', height: '32px' }}
 									src={profileCurrent.profile.image}
 								/>
-								<Typography>quanghoapq1</Typography>
-								<Typography>{articleByUser[0]?.createdAt}</Typography>
+								<Box>
+									<Typography>quanghoapq1</Typography>
+									<Typography>{articleByUser[0]?.createdAt}</Typography>
+								</Box>
 							</Box>
-							<Box className={style.userArticle}></Box>
+							<Box className={style.userArticle}>
+								<Link to="/" className={style.userArticle_title}>
+									{articleByUser[0]?.slug || 'new post'}
+								</Link>
+								<Link to="/tagList" className={style.userArticle_tagList}>
+									{articleByUser[0]?.tagList || 'tagList'}
+								</Link>
+							</Box>
+							<Box className={style.userActions} mt={4}>
+								<Box>
+									<Button>
+										<FavoriteBorderOutlined /> &nbsp; Reactions
+									</Button>
+									<Button>
+										<AddCommentOutlined /> &nbsp; Add comments
+									</Button>
+								</Box>
+
+								<Box>
+									<Button>
+										<DeleteOutlined /> &nbsp; Remove
+									</Button>
+								</Box>
+							</Box>
 						</Paper>
-					</Box>
+						<Paper className={style.ContainArticle}>
+							<Box className={style.userInfo}>
+								<Avatar
+									sx={{ width: '32px', height: '32px' }}
+									src={profileCurrent.profile.image}
+								/>
+								<Box>
+									<Typography>quanghoapq1</Typography>
+									<Typography>{articleByUser[0]?.createdAt}</Typography>
+								</Box>
+							</Box>
+							<Box className={style.userArticle}>
+								<Link to="/" className={style.userArticle_title}>
+									{articleByUser[0]?.slug || 'new post'}
+								</Link>
+								<Link to="/tagList" className={style.userArticle_tagList}>
+									{articleByUser[0]?.tagList || 'tagList'}
+								</Link>
+							</Box>
+							<Box className={style.userActions} mt={4}>
+								<Box>
+									<Button>
+										<FavoriteBorderOutlined /> &nbsp; Reactions
+									</Button>
+									<Button>
+										<AddCommentOutlined /> &nbsp; Add comments
+									</Button>
+								</Box>
+
+								<Box>
+									<Button>
+										<DeleteOutlined /> &nbsp; Remove
+									</Button>
+								</Box>
+							</Box>
+						</Paper>
+						<Paper className={style.ContainArticle}>
+							<Box className={style.userInfo}>
+								<Avatar
+									sx={{ width: '32px', height: '32px' }}
+									src={profileCurrent.profile.image}
+								/>
+								<Box>
+									<Typography>quanghoapq1</Typography>
+									<Typography>{articleByUser[0]?.createdAt}</Typography>
+								</Box>
+							</Box>
+							<Box className={style.userArticle}>
+								<Link to="/" className={style.userArticle_title}>
+									{articleByUser[0]?.slug || 'new post'}
+								</Link>
+								<Link to="/tagList" className={style.userArticle_tagList}>
+									{articleByUser[0]?.tagList || 'tagList'}
+								</Link>
+							</Box>
+							<Box className={style.userActions} mt={4}>
+								<Box>
+									<Button>
+										<FavoriteBorderOutlined /> &nbsp; Reactions
+									</Button>
+									<Button>
+										<AddCommentOutlined /> &nbsp; Add comments
+									</Button>
+								</Box>
+
+								<Box>
+									<Button>
+										<DeleteOutlined /> &nbsp; Remove
+									</Button>
+								</Box>
+							</Box>
+						</Paper>
+					</Grid>
 				</Grid>
-			</Grid>
+			</Container>
 		</Box>
 	);
 }
